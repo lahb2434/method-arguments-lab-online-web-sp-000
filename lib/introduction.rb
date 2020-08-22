eb0f5a25c8f360c33871e13e4ae46c341b4a89f4
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-22 23:01:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,9 @@ name = "Josh"
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+name = "Dan"
+language = "Ember.js"
+def introduction_to_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
